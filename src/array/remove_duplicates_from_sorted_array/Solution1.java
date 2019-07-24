7041df1376bbb3a10642d9fc18b06cd306e9613c
Harden13 <617525371@qq.com>
2019-07-24 23:05:34 +0800

    LeetCode Array Easy [ADD] remove-duplicates-from-sorted-array

---
+++ src/array/remove_duplicates_from_sorted_array/Solution1.java
@@ -0,0 +1,29 @@
+package array.remove_duplicates_from_sorted_array;
+
+/**
+ * realxz
+ * <p>
+ * 2019 - 07 -24
+ * https://leetcode-cn.com/problems/remove-duplicates-from-sorted-array
+ */
+public class Solution1 {
+    public int removeDuplicates(int[] nums) {
+        if (nums.length == 0 || nums.length == 1) {
+            return nums.length;
+        }
+
+        int position = 0;
+        int compareValue = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != compareValue) {
+                if (i != position + 1) {
+                    nums[position + 1] = nums[i];
+                }
+                position++;
+                compareValue = nums[i];
+            }
+        }
+        return position + 1;
+
+    }
+}
