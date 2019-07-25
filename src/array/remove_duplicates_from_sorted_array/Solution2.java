d244bbf461843953f2405d3afc22f2fdb0484950
Harden13 <617525371@qq.com>
2019-07-25 13:21:13 +0800

    LeetCode Array Easy [ADD]remove-duplicates-from-sorted-array solution2

---
+++ src/array/remove_duplicates_from_sorted_array/Solution2.java
@@ -0,0 +1,31 @@
+package array.remove_duplicates_from_sorted_array;
+
+
+/**
+ * realxz
+ * <p>
+ * 2019 - 07 -25
+ * https://leetcode-cn.com/problems/remove-duplicates-from-sorted-array
+ * <p>
+ * 在 {@link Solution1} 的基础上减少局部变量的创建
+ */
+public class Solution2 {
+    public int removeDuplicates(int[] nums) {
+        if (nums.length == 0 || nums.length == 1) {
+            return nums.length;
+        }
+
+        int position = 0;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[position]) {
+                if (i != position + 1) {
+                    nums[position + 1] = nums[i];
+                }
+                position++;
+                nums[position] = nums[i];
+            }
+        }
+        return position + 1;
+
+    }
+}
